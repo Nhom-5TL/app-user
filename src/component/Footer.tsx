2b95e649088aaf83a3ff72869cf1b0cf64b97e05
LyCaCa24 <datptpk03333@fpt.edu.vn>
2024-07-24 10:57:27 +0700

    Update Footer.tsx

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -7,26 +7,26 @@ const Footer = () => {
         <div className="container">
           <div className="row">
             <div className="col-sm-6 col-lg-3 p-b-50">
-              <h4 className="stext-301 cl0 p-b-30">Categories</h4>
+              <h4 className="stext-301 cl0 p-b-30">LOẠI</h4>
               <ul>
                 <li className="p-b-10">
                   <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                    Women
+                    Phụ Nữ
                   </a>
                 </li>
                 <li className="p-b-10">
                   <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                    Men
+                    Đàn Ông
                   </a>
                 </li>
                 <li className="p-b-10">
                   <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                    Shoes
+                    Đồng Hồ
                   </a>
                 </li>
                 <li className="p-b-10">
                   <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                    Watches
+                    Giày Dép
                   </a>
                 </li>
               </ul>
@@ -36,12 +36,12 @@ const Footer = () => {
               <ul>
                 <li className="p-b-10">
                   <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                    Track Order
+                  Theo dõi đơn hàng
                   </a>
                 </li>
                 <li className="p-b-10">
                   <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
-                    Returns
+                  Trở lại
                   </a>
                 </li>
                 <li className="p-b-10">
@@ -57,8 +57,8 @@ const Footer = () => {
               </ul>
             </div>
             <div className="col-sm-6 col-lg-3 p-b-50">
-              <h4 className="stext-301 cl0 p-b-30">GET IN TOUCH</h4>
-              <p className="stext-107 cl7 size-201">Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879</p>
+              <h4 className="stext-301 cl0 p-b-30">LIÊN Hệ</h4>
+              <p className="stext-107 cl7 size-201">Nếu bạn có bất kỳ câu hỏi? Hãy cho chúng tôi biết tại cửa hàng tại tầng 8, 379 Phạm Hùng, Buôn Ma Thuột hoặc gọi cho chúng tôi theo số (+84) 96 716 6879</p>
               <div className="p-t-27">
                 <a href="#" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
                   <i className="fa fa-facebook" />
@@ -72,35 +72,16 @@ const Footer = () => {
               </div>
             </div>
             <div className="col-sm-6 col-lg-3 p-b-50">
-              <h4 className="stext-301 cl0 p-b-30">Newsletter</h4>
+              <h4 className="stext-301 cl0 p-b-30">TIN TỨC</h4>
               <form>
                 <div className="wrap-input1 w-full p-b-4">
                   <input className="input1 bg-none plh1 stext-107 cl7" type="text" name="email" placeholder="email@example.com" />
                   <div className="focus-input1 trans-04" />
                 </div>
                 <div className="p-t-18">
-                  <button className="flex-c-m stext-101 cl0 size-103 bg1 bor1 hov-btn2 p-lr-15 trans-04">Subscribe</button>
+                  <button className="flex-c-m stext-101 cl0 size-103 bg1 bor1 hov-btn2 p-lr-15 trans-04">Đăng ký</button>
                 </div>
               </form>
-            </div>
-          </div>
-          <div className="p-t-40">
-            <div className="flex-c-m flex-w p-b-18">
-              <a href="#" className="m-all-1">
-                <img src="images/icons/icon-pay-01.png" alt="ICON-PAY" />
-              </a>
-              <a href="#" className="m-all-1">
-                <img src="images/icons/icon-pay-02.png" alt="ICON-PAY" />
-              </a>
-              <a href="#" className="m-all-1">
-                <img src="images/icons/icon-pay-03.png" alt="ICON-PAY" />
-              </a>
-              <a href="#" className="m-all-1">
-                <img src="images/icons/icon-pay-04.png" alt="ICON-PAY" />
-              </a>
-              <a href="#" className="m-all-1">
-                <img src="images/icons/icon-pay-05.png" alt="ICON-PAY" />
-              </a>
             </div>
           </div>
         </div>
