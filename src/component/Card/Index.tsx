83c779da3daa5f35bec6d05122a521065ff813af
TranCaoHau1 <haunaru1155@gmail.com>
2024-08-01 01:33:55 +0700

    hinh anh sản phẩm giò hàng

--- src/component/Card/Index.tsx
+++ src/component/Card/Index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import axios from 'axios';
 import { Sp } from '../api/SanPhams';
-
+export const LinkImg = "https://localhost:7095/api/SanPhams/get-pro-img/";
 const Index = () => {
   const [sansp, setSanPhams] = useState<Sp[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
@@ -46,10 +46,10 @@ const Index = () => {
       <div className="container mt-5">
         <div className="bread-crumb flex-w p-l-25 p-r-15 p-t-30 p-lr-0-lg">
           <a href="index.html" className="stext-109 cl8 hov-cl1 trans-04">
-            Home
+            Trang chủ
             <i className="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true" />
           </a>
-          <span className="stext-109 cl4">Shopping Cart</span>
+          <span className="stext-109 cl4">Giỏ hàng</span>
         </div>
       </div>
       <form className="bg0 p-t-75 p-b-85">
