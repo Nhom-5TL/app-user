3e983058afddf7d43dd23fba34f9cb9d5903c188
TranCaoHau1 <haunaru1155@gmail.com>
2024-08-01 01:38:21 +0700

    sửa banner

--- src/component/Home/Baner.tsx
+++ src/component/Home/Baner.tsx
@@ -13,10 +13,10 @@ const Baner = () => {
                 <a href="product.html" className="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                   <div className="block1-txt-child1 flex-col-l">
                     <span className="block1-name ltext-102 trans-04 p-b-8">Women</span>
-                    <span className="block1-info stext-102 trans-04">Spring 2018</span>
+                    <span className="block1-info stext-102 trans-04">Đồng hồ nữ</span>
                   </div>
                   <div className="block1-txt-child2 p-b-4 trans-05">
-                    <div className="block1-link stext-101 cl0 trans-09">Shop Now</div>
+                    <div className="block1-link stext-101 cl0 trans-09">Mua ngay</div>
                   </div>
                 </a>
               </div>
@@ -28,10 +28,10 @@ const Baner = () => {
                 <a href="product.html" className="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                   <div className="block1-txt-child1 flex-col-l">
                     <span className="block1-name ltext-102 trans-04 p-b-8">Men</span>
-                    <span className="block1-info stext-102 trans-04">Spring 2018</span>
+                    <span className="block1-info stext-102 trans-04">Đồng hồ nam</span>
                   </div>
                   <div className="block1-txt-child2 p-b-4 trans-05">
-                    <div className="block1-link stext-101 cl0 trans-09">Shop Now</div>
+                    <div className="block1-link stext-101 cl0 trans-09">Mua ngay</div>
                   </div>
                 </a>
               </div>
@@ -39,14 +39,14 @@ const Baner = () => {
             <div className="col-md-6 col-xl-4 p-b-30 m-lr-auto">
               {/* Block1 */}
               <div className="block1 wrap-pic-w">
-                <img src="src/assets/images/banner-03.jpg" alt="IMG-BANNER" />
+                <img src="src/assets/images/banner-07.jpg" alt="IMG-BANNER" />
                 <a href="product.html" className="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                   <div className="block1-txt-child1 flex-col-l">
-                    <span className="block1-name ltext-102 trans-04 p-b-8">Accessories</span>
+                    <span className="block1-name ltext-102 trans-04 p-b-8">Đồng hồ dây da</span>
                     <span className="block1-info stext-102 trans-04">New Trend</span>
                   </div>
                   <div className="block1-txt-child2 p-b-4 trans-05">
-                    <div className="block1-link stext-101 cl0 trans-09">Shop Now</div>
+                    <div className="block1-link stext-101 cl0 trans-09">Mua ngay</div>
                   </div>
                 </a>
               </div>
