56e106b271970351ec89bc0d22e610b34dd3174b
TranCaoHau1 <haunaru1155@gmail.com>
2024-08-14 20:31:19 +0700

    update lần 2

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -125,10 +125,6 @@ const Header = () => {
                   ): (
                     ""
                   )}
-                  
-                  <li><a href="blog.html">Blog</a></li>
-                  <li><a href="about.html">About</a></li>
-                  <li><a href="contact.html">Contact</a></li>
                 </ul>
               </div>
               {/* Icon header */}
